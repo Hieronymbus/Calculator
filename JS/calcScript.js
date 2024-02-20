55d48241d42d8109e5c2127ed8b6b3d795bada4a
Hieronymbuse <birdsallalexander1990@gmail.comm>
2024-02-20 15:41:47 +1300

    2nd commit 3b

--- JS/calcScript.js
+++ JS/calcScript.js
@@ -1,5 +1,10 @@
 function clearDisplay() {
-    document.getElementById("output_text_container").textContent = "0"
+    document.getElementById("output_text_container").textContent = "0" ;
 }
 
+function inputNumber(number) {
+    let input = document.getElementById("output_text_container").textContent ;
+    document.getElementById("output_text_container").textContent = input + number
+    
+}
 
