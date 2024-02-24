6292527fa8e9c1b77904975cfb329f5ef7d6127f
alex birdsall <birdsallalexander1990@gmail.com>
2024-02-24 16:19:38 +1300

    homework commit added many functions

--- JS/calcScript.js
+++ JS/calcScript.js
@@ -1,10 +1,18 @@
+
+
 function clearDisplay() {
     document.getElementById("output_text_container").textContent = "0" ;
 }
 
-function inputNumber(number) {
-    let input = document.getElementById("output_text_container").textContent ;
-    document.getElementById("output_text_container").textContent = input + number
-    
+function inputButton(input) {
+    if (document.getElementById("output_text_container").textContent == 0) {
+       document.getElementById("output_text_container").textContent = input
+    } else {
+        document.getElementById("output_text_container").textContent += input
+    }
+}
+
+function calculate() {
+    document.getElementById("output_text_container").textContent = eval(document.getElementById("output_text_container").textContent).toFixed(2)
 }
 
